f205cde601580bfcc7fcdb31e11734ebe18a745d
Maria Clara Colaço <colacoclara0@gmail.com>
2026-04-01 10:37:09 -0300

    feat: atualiza tb_top.sv com a instância do DUT e testes

--- prj/hello/tb/tb_top.sv
+++ prj/hello/tb/tb_top.sv
@@ -1,7 +1,31 @@
 module tb_top();
+  logic a, b, c;
+
+  // Instância do DUT
+  dut uu_dut (
+    .a(a),
+    .b(b),
+    .c(c)
+  );
+
   initial begin
-    repeat(10) begin
-      $display("Hello, VCS!");
-    end
+    // Exibindo as combinações de entradas
+    $display("a b | c");
+    $display("-----");
+
+    // Testando todas as combinações de entrada
+    a = 0; b = 0; #1ns;
+    $display("%0d %0d | %0d", a, b, c);
+
+    a = 0; b = 1; #1ns;
+    $display("%0d %0d | %0d", a, b, c);
+
+    a = 1; b = 0; #1ns;
+    $display("%0d %0d | %0d", a, b, c);
+
+    a = 1; b = 1; #1ns;
+    $display("%0d %0d | %0d", a, b, c);
+
+    $finish;
   end
 endmodule
